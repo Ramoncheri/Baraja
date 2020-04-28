4e3f41facc468fb74a8d4bf07e62513cd02ee028
Mon Gomez <nulo76@yahoo.es>
2020-04-28 14:12:28 +0100

    funcion barajar

--- cartas.py
+++ cartas.py
@@ -1,3 +1,5 @@
+import random
+
 palos=('0','c','e','b')
 cartas=('A','2','3','4','5','6','7','S','C','R')
 
@@ -12,4 +14,22 @@ def creaBaraja():
             baraja.append(naipe)
 
     return baraja
+
+def barajar(b):
+    
+    bBarajada=[]
+    for item in b:
+        ind= random.randint(0,39)
+        cartaBaraj= b[ind]
+        
+        while cartaBaraj in bBarajada:
+            ind= random.randint(0,39)
+            cartaBaraj= b[ind]
+
+        bBarajada.append(cartaBaraj)
+    
+
+    return bBarajada
+
+
 
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import cartas
 
 b1= cartas.creaBaraja()
+bB= cartas.barajar(b1)
 
 print(b1)
+print(bB)
+
+
+
