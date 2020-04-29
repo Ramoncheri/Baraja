3a33cf7aa8c80f1a7d077bdb5724df0f70125e5c
Mon Gomez <nulo76@yahoo.es>
2020-04-29 13:26:55 +0100

    ahora si, final commit

--- cCartas.py
+++ cCartas.py
@@ -34,15 +34,32 @@ class Baraja():
 
     def repartir(self, players,cards):
         
-        for p in range(players):
-            self.mano.append([])  #crea tantas listas vacias como jugadores
+        if self.mano == []:
+            for p in range(players):
+                self.mano.append([])  #crea tantas listas vacias como jugadores
+        else:
+            pass
         
         for ic in range (cards):
-            for ij in range (players):
-                carta = self.naipes.pop(0)
-                self.mano[ij].append(carta)
+                for ij in range (players):
+                    carta = self.naipes.pop(0)
+                    self.mano[ij].append(carta)
 
         return self.mano
     
     def recoger(self):
         self.__creaBaraja()
+
+if __name__== "__main__":
+
+    b1= Baraja()
+    print (b1.naipes)
+    b1.barajar()
+    bR= b1.repartir(3, 5)
+
+    print(b1.naipes)
+    #print(bB)
+    print(bR)
+
+    bRR= b1.repartir(3,1)
+    print (bRR)
